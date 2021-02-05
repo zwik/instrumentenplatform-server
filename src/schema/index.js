4a89ff6ad7e4cb08bb79dc0de6fef7683b60a35e
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2021-02-05 15:20:41 +0100

    Refactor graphql schema

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,14 +1,14 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
+  type Query {
+    temperature: [Temperature]
+  }
+
   type Temperature {
     datetime: Float
     tempvalue: Float,
     feelslike: Float,
     dewpoint: Float
-  }
-
-  type Query {
-    temperature: [Temperature]
   }
 `;
