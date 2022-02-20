84c3b73b572a3b81f3170b170339e86249ad1c1d
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2022-02-20 11:52:54 +0100

    Add link from schema to datasources for missing API

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,7 +1,11 @@
 /* eslint-disable max-len */
 module.exports = {
-  temperature: (_parent, _args, { dataSources }) => dataSources.temperatureAPI.getAllTemperatures(),
-  temperatureRange: (_parent, { from, to }, { dataSources }) => dataSources.temperatureAPI.getTemperatureRange(from, to),
   air: (_parent, _args, { dataSources }) => dataSources.airAPI.getAllAir(),
   airRange: (_parent, { from, to }, { dataSources }) => dataSources.airAPI.getAirRange(from, to),
+  rain: (_parent, _args, { dataSources }) => dataSources.rainAPI.getAllRain(),
+  rainRange: (_parent, { from, to }, { dataSources }) => dataSources.rainAPI.getRainRange(from, to),
+  sun: (_parent, _args, { dataSources }) => dataSources.sunAPI.getAllSun(),
+  sunRange: (_parent, { from, to }, { dataSources }) => dataSources.sunAPI.getSunRange(from, to),
+  temperature: (_parent, _args, { dataSources }) => dataSources.temperatureAPI.getAllTemperatures(),
+  temperatureRange: (_parent, { from, to }, { dataSources }) => dataSources.temperatureAPI.getTemperatureRange(from, to),
 };
